98802cbedb7730c62673588fcc27af9a2ad26db0
ahmedreda6 <ahmed.reda55767@gmail.com>
2024-05-03 15:38:14 +0300

    best seller list view item part1

--- lib/Features/home/presentaion/views/widgets/home_view_body.dart
+++ lib/Features/home/presentaion/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:booklyapp/Features/home/presentaion/views/widgets/custom_app_bar.dart';
 import 'package:booklyapp/Features/home/presentaion/views/widgets/featured_list_view.dart';
+import 'package:booklyapp/core/utils/assets_data.dart';
 import 'package:booklyapp/core/utils/styles.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -22,6 +23,40 @@ class HomeViewBody extends StatelessWidget {
           Text(
             'Best Seller',
             style: Styles.titelMedium,
+          ),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 130,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.7 / 4,
+            child: Container(
+              width: 100,
+              height: MediaQuery.of(context).size.height * 0.25,
+              decoration: BoxDecoration(
+                color: Colors.amber,
+                borderRadius: BorderRadius.circular(8),
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.testImage),
+                ),
+              ),
+            ),
+          ),
+          const Column(
+            children: [],
           )
         ],
       ),
