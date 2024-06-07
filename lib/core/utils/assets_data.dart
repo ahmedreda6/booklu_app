b79f01bd447fe8338254933815436a04f3506e35
ahmedreda6 <ahmed.reda55767@gmail.com>
2024-06-07 17:54:58 +0300

    .

--- lib/core/utils/assets_data.dart
+++ lib/core/utils/assets_data.dart
@@ -1,4 +1,5 @@
 class AssetsData {
   static const logo = 'assets/images/Logo.png';
-  static const testImage = 'assets/images/Book 1 Hightligh.png';
+  static const testImage =
+      'assets/images/harry-potter-and-the-chamber-of-secrets-6.png';
 }
