72e72cf7068ac34976e0dfb6b559f378ab6151d8
ahmedreda6 <ahmed.reda55767@gmail.com>
2024-06-12 06:17:29 +0300

    create custom search text field

--- lib/Features/search/presentaion/views/search_view.dart
+++ lib/Features/search/presentaion/views/search_view.dart
@@ -6,8 +6,10 @@ class SearchView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(child: SearchViewBody()),
+    return const Scaffold(
+      body: SafeArea(
+        child: SearchViewBody(),
+      ),
     );
   }
 }
