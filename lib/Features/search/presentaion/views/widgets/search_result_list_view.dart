bb0e989e3953ecc404671ea56ee83a36add28fb9
ahmedreda6 <ahmed.reda55767@gmail.com>
2024-06-12 06:29:50 +0300

    search view done

--- lib/Features/search/presentaion/views/widgets/search_result_list_view.dart
+++ lib/Features/search/presentaion/views/widgets/search_result_list_view.dart
@@ -1,13 +1,12 @@
 import 'package:booklyapp/Features/home/presentaion/views/widgets/best_seller_list_view_item.dart';
 import 'package:flutter/material.dart';
 
-class BestSellerListView extends StatelessWidget {
-  const BestSellerListView({super.key});
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      physics: const NeverScrollableScrollPhysics(),
       padding: EdgeInsets.zero,
       itemCount: 10,
       itemBuilder: (context, index) {
--- lib/Features/search/presentaion/views/widgets/search_view_body.dart
+++ lib/Features/search/presentaion/views/widgets/search_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:booklyapp/Features/search/presentaion/views/widgets/custom_search_text_field.dart';
+import 'package:booklyapp/Features/search/presentaion/views/widgets/search_result_list_view.dart';
+import 'package:booklyapp/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -9,10 +11,26 @@ class SearchViewBody extends StatelessWidget {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 30),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomSearchTextFiled(),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            'Search result',
+            style: Styles.textStyle18,
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Expanded(
+            child: SearchResultListView(),
+          )
         ],
       ),
     );
   }
 }
+
+
