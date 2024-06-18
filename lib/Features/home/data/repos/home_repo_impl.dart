da19a5547dc570ac2d70023800cdcacd4cb75646
ahmedreda6 <ahmed.reda55767@gmail.com>
2024-06-18 07:41:00 +0300

    impl featured box

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -36,8 +36,28 @@ class HomeRepoImpl implements HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBox() {
-    // TODO: implement fetchFeaturedBox
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBox() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&q=subject:Programming');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.DioException(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
   }
 }
