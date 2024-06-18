a1ded6da3ff53fb4e1826a9d53172a86ee6896ce
ahmedreda6 <ahmed.reda55767@gmail.com>
2024-06-18 06:13:39 +0300

    create api service

---
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,18 @@
+import 'package:booklyapp/Features/home/data/models/book_model/book_model.dart';
+import 'package:booklyapp/Features/home/data/repos/home_repo.dart';
+import 'package:booklyapp/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl implements HomeRepo{
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
+    // TODO: implement fetchBestSellerBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBox() {
+    // TODO: implement fetchFeaturedBox
+    throw UnimplementedError();
+  }
+}
